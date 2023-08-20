5e976483436c6f0f20ce737454cc8350af374ac4
HayataTakagi <h.takagi.work@gmail.com>
2023-08-20 17:42:43 +0900

    add buffer

--- src/disk.rs
+++ src/disk.rs
@@ -1,25 +1,58 @@
 use std::{
     fs::{File, OpenOptions},
-    io::{Read, Result, Seek, SeekFrom, Write},
+    io::{self, Read, Result, Seek, SeekFrom, Write},
     path::Path,
 };
 
-const PAGE_SIZE: u64 = 4096;
+use zerocopy::{AsBytes, FromBytes};
 
-pub struct DisManeger {
+pub const PAGE_SIZE: usize = 4096;
+
+pub struct DiskManager {
     heap_file: File,
     next_page_id: u64,
 }
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, FromBytes, AsBytes)]
+#[repr(C)]
 pub struct PageId(pub u64);
 
 impl PageId {
+    pub const INVALID_PAGE_ID: PageId = PageId(u64::MAX);
+
+    pub fn valid(self) -> Option<PageId> {
+        if self == Self::INVALID_PAGE_ID {
+            None
+        } else {
+            Some(self)
+        }
+    }
+
     pub fn to_u64(&self) -> u64 {
         self.0 as u64
     }
 }
 
-impl DisManeger {
+impl Default for PageId {
+    fn default() -> Self {
+        Self::INVALID_PAGE_ID
+    }
+}
+
+impl From<Option<PageId>> for PageId {
+    fn from(page_id: Option<PageId>) -> Self {
+        page_id.unwrap_or_default()
+    }
+}
+
+impl From<&[u8]> for PageId {
+    fn from(bytes: &[u8]) -> Self {
+        let arr = bytes.try_into().unwrap();
+        PageId(u64::from_ne_bytes(arr))
+    }
+}
+
+impl DiskManager {
     pub fn new(heap_file: File) -> Result<Self> {
         // ファイルサイズを取得
         let heap_file_size = heap_file.metadata()?.len();
@@ -62,18 +95,23 @@ impl DisManeger {
         // データを読み出す
         self.heap_file.read_exact(data)
     }
+
+    pub fn sync(&mut self) -> io::Result<()> {
+        self.heap_file.flush()?;
+        self.heap_file.sync_all()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::io::Read;
 
-    use crate::disk::DisManeger;
+    use crate::disk::DiskManager;
 
     #[test]
     fn open_ファイルが開けること() {
         let test_file_path = "src/tests/sample.txt";
-        let dm_reslt = DisManeger::open(test_file_path);
+        let dm_reslt = DiskManager::open(test_file_path);
 
         assert_eq!(dm_reslt.is_ok(), true);
         let mut dm = dm_reslt.unwrap();
